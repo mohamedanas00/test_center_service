9915ca32d749880b150ec46e319cea94ebac7c65
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-05-12 03:35:08 +0300

    add logs for testCenter

--- src/modules/schedule/controller/schedule.js
+++ src/modules/schedule/controller/schedule.js
@@ -1,10 +1,12 @@
 import { StatusCodes } from "http-status-codes";
 import scheduleModel from "../../../../DB/models/schedule.model.js";
 import { asyncHandler } from "../../../utils/errorHandling.js";
+import logsModel from "../../../../DB/models/logs.model.js";
 
 export const createSchedule = asyncHandler(async (req, res) => {
   const { date, time, capacity } = req.body;
   const { examId, branchId } = req.query;
+  const testCenter = req.user;
   const newSchedule = await scheduleModel.create({
     examId,
     branchId,
@@ -12,6 +14,12 @@ export const createSchedule = asyncHandler(async (req, res) => {
     time,
     capacity,
   });
+  await logsModel.create({
+    userId: testCenter.id,
+    email: testCenter.email,
+    role: testCenter.role,
+    action: `Create Exam Schedule successfully with id ${newSchedule._id}`,
+})
   res
     .status(StatusCodes.CREATED)
     .json({ message: "Schedule created successfully", newSchedule });
